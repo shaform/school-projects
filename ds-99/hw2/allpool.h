083492df3ee6ef23268cc81949f3a3255665d0d9
Shaform <shaform@gmail.com>
2010-10-09 18:07:55 +0800

    Added DEBUG support & fixed acquire problem.

--- ds-99/hw2/allpool.h
+++ ds-99/hw2/allpool.h
@@ -8,71 +8,31 @@ namespace HOMEWORK
 
 
 	// Node management
-	struct header {
-		sz_t sz;
-
-		sz_t up;
-		sz_t down;
-
-		sz_t larger;
-		sz_t lesser;
-	}; 
+	struct header { sz_t sz, up, down, larger, lesser; }; 
 	
 	const size_t HEADER = sizeof(header);
 	const size_t FOOTER = sizeof(sz_t);
 	const size_t DATA = HEADER + FOOTER;
-
 	const size_t BOUND = DATA;
 	const size_t LOW = BOUND;
 	const ptn ACQUIRED = 0u;
 	const ptn NOTINLIST = ACQUIRED-1u;
 
-
-
 	ptn root, tail;
 
-
-	inline ptn get_ptn(void *addr)
-	{
-		return ((sz_t ) addr) - HEADER;
-	}
-	inline void* get_mem(ptn u)
-	{
-		return (void *) (u + HEADER);
-	}
-	inline sz_t &get_size(ptn ptr)
-	{
-		return *((sz_t *) ptr);
-	}
-	inline ptn &get_down(ptn ptr)
-	{
-		return ((header *) ptr)->down;
-	}
-	inline ptn &get_up(ptn ptr)
-	{
-		return ((header *) ptr)->up;
-	}
-	inline ptn &get_larger(ptn ptr)
-	{
-		return ((header *) ptr)->larger;
-	}
-	inline ptn &get_lesser(ptn ptr)
-	{
-		return ((header *) ptr)->lesser;
-	}
-	inline void set_second_size(ptn ptr)
-	{
+	inline ptn get_ptn(void *addr) { return ((sz_t ) addr) - HEADER; }
+	inline void* get_mem(ptn u) { return (void *) (u + HEADER); }
+	inline sz_t &get_size(ptn ptr) { return *((sz_t *) ptr); }
+	inline ptn &get_down(ptn ptr) { return ((header *) ptr)->down; }
+	inline ptn &get_up(ptn ptr) { return ((header *) ptr)->up; }
+	inline ptn &get_larger(ptn ptr) { return ((header *) ptr)->larger; }
+	inline ptn &get_lesser(ptn ptr) { return ((header *) ptr)->lesser; }
+	inline void set_second_size(ptn ptr) {
 		sz_t sz = get_size(ptr);
 		*((sz_t *) (ptr + HEADER + sz)) = sz;
 	}
-	inline ptn get_next(ptn u)
-	{
-		return u + DATA + get_size(u);
-	}
-	inline ptn get_prev(ptn u)
-	{
-		return u - *((sz_t *) (u-FOOTER)) - DATA;
-	}
+	inline ptn get_next(ptn u) { return u + DATA + get_size(u); }
+	inline ptn get_prev(ptn u) { return u - *((sz_t *) (u-FOOTER)) - DATA; }
 
 #if DEBUG
 #include <cstdio>
@@ -84,6 +44,17 @@ namespace HOMEWORK
 			printf("#%llu : %llu\n", it, get_size(it));
 			it = get_next(it);
 		}
+		it = get_larger(root);
+		while (it != root) {
+			ptn it2 = it;
+			printf("#size: %llu\n", get_size(it));
+			while (it2 != root) {
+				printf("#%llu : %llu\n", it2, get_size(it2));
+				it2 = get_down(it2);
+			}
+			it = get_larger(it);
+		}
+
 	}
 #endif
 
@@ -96,10 +67,7 @@ namespace HOMEWORK
 
 	// Free list
 	
-	inline bool is_top(ptn u)
-	{
-		return get_up(u) == root;
-	}
+	inline bool is_top(ptn u) { return get_up(u) == root; }
 
 	inline void pop(ptn u)
 	{
@@ -180,18 +148,9 @@ namespace HOMEWORK
 
 		return u;
 	}
-	inline void delete_f(ptn u)
-	{
-		get_down(u) = ACQUIRED;
-	}
-	inline bool is_free(ptn u)
-	{
-		return get_down(u) != ACQUIRED;
-	}
-	inline bool in_lst(ptn u)
-	{
-		return get_down(u) != NOTINLIST;
-	}
+	inline void delete_f(ptn u) { get_down(u) = ACQUIRED; }
+	inline bool is_free(ptn u) { return get_down(u) != ACQUIRED; }
+	inline bool in_lst(ptn u) { return get_down(u) != NOTINLIST; }
 
 
 	inline ptn insert_f(ptn u)
@@ -253,7 +212,7 @@ namespace HOMEWORK
 	inline void* acquire(unsigned long long nSize)
 	{
 
-		ptn it = get_larger(root);
+		ptn it = get_lesser(root);
 
 		sz_t sz;
 		while ((sz = get_size(it)) > nSize)
--- ds-99/hw2/tester.cpp
+++ ds-99/hw2/tester.cpp
@@ -7,6 +7,12 @@
 using namespace std;
 using namespace HOMEWORK;
 
+#if DEBUG
+#define	CHECK check_list()
+#else
+#define	CHECK  
+#endif
+
 int main()
 {
 
@@ -15,13 +21,19 @@ int main()
 
 	// Large memory check
 	int *t = (int *)acquire(4194000);
+	CHECK;
 	int *u = (int *)acquire(sizeof(int)*300);
+	CHECK;
 	if (u==0) {
 		release((void *)t);
+		CHECK;
 		t = (int *)acquire(5*sizeof(int));
+		CHECK;
 		u = (int *)acquire(sizeof(int)*3);
+		CHECK;
 	}
 	int *v = (int *)acquire(sizeof(int)*4);
+	CHECK;
 
 	// Check for memory validity
 	for (int i=0; i<5; ++i)
@@ -32,16 +44,27 @@ int main()
 		v[i] = i;
 
 	release((void *)t);
+	CHECK;
 	t = (int *)acquire(sizeof(int)*3);
+	CHECK;
 	int *w = (int *)acquire(sizeof(int)*2);
+	CHECK;
 	release((void *)w);
+	CHECK;
 	w = (int *)acquire(sizeof(int)*2);
+	CHECK;
 	release((void *)w);
+	CHECK;
 	w = (int *)acquire(sizeof(int)*20);
+	CHECK;
 	release((void *)t);
+	CHECK;
 	release((void *)u);
+	CHECK;
 	release((void *)v);
+	CHECK;
 	release((void *)w);
+	CHECK;
 
 
 	// random check
@@ -61,7 +84,59 @@ int main()
 		vec.pop_back();
 	}
 
+	// Reallocate Check
+	void *r[100];
+	r[0] = acquire(1000);
+	CHECK;
+	r[1] = acquire(2000);
+	CHECK;
+	r[2] = acquire(3000);
+	CHECK;
+	r[3] = acquire(4000);
+	CHECK;
+	r[4] = acquire(5000);
+	CHECK;
+	r[5] = acquire(6000);
+	CHECK;
+	release(r[2]);
+	CHECK;
+	release(r[4]);
+	CHECK;
+	r[2] = acquire(500);
+	CHECK;
+	r[4] = acquire(500);
+	CHECK;
+	r[6] = acquire(500);
+	CHECK;
+	r[7] = acquire(500);
+	CHECK;
+	r[8] = acquire(1500);
+	CHECK;
+	r[9] = acquire(2500);
+	CHECK;
+	r[10] = acquire(5500);
+	CHECK;
+	release(r[0]);
+	CHECK;
+	release(r[1]);
+	CHECK;
+	release(r[7]);
+	CHECK;
+	release(r[8]);
+	CHECK;
+	r[0] = acquire(1000);
+	CHECK;
+	r[1] = acquire(2000);
+	CHECK;
+	r[7] = acquire(500);
+	CHECK;
+	r[8] = acquire(1500);
+	CHECK;
 
+	for (int i=10; i>= 0; --i) {
+		release(r[i]);
+		CHECK;
+	}
 
 	operator delete(test);
 }
