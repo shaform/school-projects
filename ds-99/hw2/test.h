3d1dc927c3715490cf225c31e21700d37b25eb40
Shaform <shaform@gmail.com>
2011-08-18 20:25:29 +0800

    Add a fun version of hw2.

---
+++ ds-99/hw2/test.h
@@ -0,0 +1,95 @@
+// Just for fun test =D
+#define AUA return
+#define IoI if
+#define O_OoC static
+#define OuOuy else
+#define QjQ const
+#define o_oa int
+#define VoV void
+#define Fo_oF for
+#define Mo_oM while
+#define B_B bool
+#define UO_OU unsigned
+#define O_O_____O long
+#define TyT typedef
+#define PaP namespace
+#define SoS struct
+#define zzZ sizeof
+#pragma once
+PaP HOMEWORK{TyT UO_OU O_O_____O O_O_____O 
+o3o;TyT UO_OU O_O_____O O_O_____O oToTO;TyT 
+o3o oO_OO;
+SoS OO_Oo{oToTO ukp323,
+xau,uqa,paa,
+kko;};QjQ o_oa KK=zzZ(
+OO_Oo);QjQ o_oa II=zzZ(
+oToTO);QjQ o_oa UA=KK+ II;QjQ o_oa XXXE=UA;QjQ 
+o_oa IUKKO=XXXE;QjQ oO_OO 
+AAAAA=0u;QjQ oO_OO UKX=
+AAAAA-1u;QjQ oO_OO PKU=
+UKX-1u; QjQ o_oa nctu=(0xcc0+649-0xf42);
+oToTO HAHA[nctu]={
+(0x456+6567-0x1dfd),(0xe2+5807-0x172d),(0x880+2624-0x11f8),(0x289+7606-0x1f13),
+(0xcbf+5151-0x1eea),(0x1443+2841-0x1980),(0x1a4b+7524-0x2427)};
+O_OoC o_oa cscs(oToTO ukp323){o_oa kuku;Fo_oF(kuku=nctu-(0x788+2894-0x12d5);
+HAHA[kuku]>ukp323;--kuku); AUA kuku;}oO_OO A_A[
+nctu],uuuu;O_OoC oO_OO OjjO(VoV*yyyyy){AUA((oToTO)
+yyyyy)-KK;}O_OoC VoV*mOuOm( oO_OO kerker){AUA(VoV*)(kerker+ KK);}O_OoC oToTO&XD(
+oO_OO oAO){AUA*((oToTO*) oAO);}O_OoC oO_OO&TOT( oO_OO oAO){AUA((OO_Oo*)
+oAO)->uqa;}O_OoC oO_OO& OuO(oO_OO oAO){AUA((
+OO_Oo*)oAO)->xau;}O_OoC oO_OO&OTZ(oO_OO 
+oAO){AUA((OO_Oo*)oAO)-> paa;}O_OoC oO_OO&TAT( oO_OO oAO){AUA((OO_Oo*)
+oAO)->kko;}O_OoC VoV alalaletaaa(oO_OO oAO){
+oToTO ukp323=XD(oAO);*((oToTO*)(oAO+KK+
+ukp323))=ukp323;}O_OoC oO_OO 
+O_Oa(oO_OO kerker){AUA kerker+UA+XD(kerker);}
+O_OoC oO_OO Q_O(oO_OO kerker){AUA kerker-*((oToTO*)(
+kerker-II))-UA;}O_OoC VoV QoQ(oO_OO kerker,
+oToTO ukp323){XD(kerker)
+=ukp323;alalaletaaa(kerker);} O_OoC B_B kukusu33aoby(oO_OO kerker){AUA 
+OuO(kerker)==PKU;}O_OoC VoV WOW(oO_OO kerker){oO_OO&
+QAq=TOT(kerker);IoI(QAq!=
+PKU)OuO(QAq)= PKU;oO_OO paa= OTZ(kerker),kko= TAT(kerker);IoI(QAq== PKU){OTZ(kko)=
+paa;TAT(paa)= kko;}OuOuy{OTZ(QAq)= paa;TAT(QAq)=
+kko;OTZ(kko)= QAq;TAT(paa)= QAq;}QAq=UKX;}O_OoC VoV 
+O_Q(oO_OO kerker){IoI( kukusu33aoby(kerker))WOW(kerker); OuOuy{oO_OO xau=OuO(kerker
+),uqa=TOT(kerker); TOT(xau)=uqa;IoI( uqa!=PKU)OuO(
+uqa)=xau;TOT(kerker)
+=UKX;}}O_OoC VoV GJ( oO_OO kerker,o_oa yooooooo){oToTO 
+ukp323=XD(kerker),XPsz;
+oO_OO XP=TAT(
+A_A[yooooooo]);Mo_oM((XPsz= XD(XP))>ukp323)XP= TAT(XP);IoI(XPsz==
+ukp323){oO_OO eu32u3=TOT(
+XP);OuO(kerker)=XP; TOT(kerker)=eu32u3; TOT(XP)=kerker;IoI(eu32u3
+!=PKU)OuO(eu32u3)= kerker;}OuOuy{oO_OO paa= OTZ(XP);;TAT(
+kerker)=XP;OTZ(kerker)=
+paa;OTZ(XP)= kerker;TAT(paa)= kerker;TOT(kerker)=PKU
+;OuO(kerker)=PKU;}} O_OoC oO_OO Y_Y(oO_OO kerker,oO_OO OvO){
+QoQ(kerker,XD( kerker)+XD(OvO)+UA) ;AUA kerker;}O_OoC VoV OooO(oO_OO
+ kerker){TOT(kerker)=
+AAAAA;}O_OoC B_B o_o(oO_OO 
+kerker){AUA TOT(kerker)!= AAAAA;}O_OoC B_B kukundfh65(oO_OO kerker){AUA TOT(kerker)!=
+UKX;}O_OoC oO_OO lol(
+oO_OO kerker){TOT(kerker)= UKX;oO_OO alalal= Q_O(kerker);IoI(o_o(
+alalal)){IoI(kukundfh65(alalal)) O_Q(alalal);kerker=
+Y_Y(alalal,kerker);}alalal= O_Oa(kerker);IoI(o_o( alalal)){IoI(kukundfh65(alalal)) O_Q(alalal);kerker=
+Y_Y(kerker,alalal);}AUA kerker
+;}O_OoC VoV initialize_pool(VoV*yyyyy,
+oToTO orz){ orz-=UA*(nctu+ (0x3a2+3262-0x105e));oO_OO XP=(oO_OO)
+yyyyy; Fo_oF(o_oa kuku=(0xef7+3819-0x1de2);kuku<
+nctu;++kuku){A_A[kuku]= XP;QoQ(A_A[kuku
+],(0x2c1+7417-0x1fba));TOT(A_A[ kuku])=AAAAA;OTZ( A_A[kuku])=A_A[kuku]; TAT(A_A[kuku])= A_A[kuku];XP=O_Oa( XP);}
+oO_OO QQ=XP; QoQ(QQ,orz); GJ(QQ,cscs( orz));
+uuuu=O_Oa(QQ); QoQ(uuuu,(0x18cc+3255-0x2583));
+TOT(uuuu)=AAAAA;}O_OoC VoV*acquire(UO_OU O_O_____O O_O_____O orz){o_oa yooooooo=cscs(orz);oO_OO 
+XP;XP=TAT( A_A[yooooooo]);Mo_oM(XD(
+XP)>=orz)XP= TAT(XP);XP= OTZ(XP);Mo_oM(XP== A_A[yooooooo]&&++yooooooo<nctu)
+XP=OTZ(A_A[yooooooo ]);IoI(yooooooo==nctu)AUA(0x45c+6212-0x1ca0); oToTO ukp323=XD(XP)
+;WOW(XP);OooO(XP);
+IoI(ukp323>orz+UA+ XXXE){QoQ(XP, orz);oO_OO QAq=
+O_Oa(XP);ukp323-= orz+UA;yooooooo=cscs( ukp323);QoQ(QAq,ukp323)
+;QAq=lol(QAq); GJ(QAq,yooooooo);}AUA mOuOm(XP);}O_OoC VoV release(
+VoV*yyyyy){oO_OO XP=
+OjjO(yyyyy);XP= lol(XP);TOT(
+XP)=UKX;o_oa ukp323= XD(XP);IoI(ukp323>
+XXXE)GJ(XP, cscs(ukp323));}}
