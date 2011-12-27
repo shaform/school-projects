bcffe7998ad772508e8560d3e32920fcfb30342d
Shaform <shaform@gmail.com>
2011-12-27 21:49:25 +0800

    Add 10 questions.

--- micro-100/questions.c
+++ micro-100/questions.c
@@ -3,8 +3,7 @@
 #include "display.h"
 #include "questions.h"
 
-#define QN 6
-#define MAXN 100
+#define QN 16
 
 uchar qnum;
 code struct Question {
@@ -19,8 +18,27 @@ code struct Question {
 		0
 	},
 	{
+		"請問哪一位不是交大資工系的教授(包含副教授、副教授)?\r\n"
+			"(A) 陳榮傑 (B) 徐慰中 (C) 傅心家 (D) 陸曉風\r\n",
+		4,
+		0
+	},
+	{
+		"4 9 5 7 6 5 7 _\r\n"
+			"根據以上的數列,_的數字最有可能是\r\n"
+			"(A) 3 (B) 4 (C) 7 (D) 8\r\n",
+		1,
+		0
+	},
+	{
 		"微處理機實驗共有幾個Lab?\r\n"
 			"(A) 99  (B) 49  (C) 13  (D) 10\r\n",
+		4,
+		0
+	},
+	{
+		"目前立委大選共選出幾人?\r\n"
+			"(A) 225 (B) 234 (C) 245 (D) 113 人\r\n",
 		4,
 		0
 	},
@@ -31,10 +49,35 @@ code struct Question {
 		0
 	},
 	{
+		"2011年12月19日金正日逝世，請問目前他的接班人是?\r\n"
+			"(A) 金正哲 (B) 金正恩 (C) 金正男 (D) 張成澤\r\n",
+		2,
+		0
+	},
+	{
 		"請問這是哪部作品的歌曲?\r\n"
 			"(A) 那些年，我們一起追的女孩  (B) 小資女孩向前衝  (C) 犀利人妻  (D) 我可能不會愛你\r\n",
 		3,
 		1
+	},
+	{
+		"在600個家庭中，150個家庭沒有小孩，200個家庭只有1個男孩，200個家庭只有一個女孩，請問在有幾個家庭有男孩也有女孩?\r\n"
+			"(A) 350 (B) 250 (C) 50 (D) 0\r\n",
+		3,
+		0
+	},
+	{
+		"2012年(第十三任)總統大選，國民黨、民進黨、親民黨的總統候選人分別是:\r\n"
+			"(A) 吳敦義、蔡英文、宋楚瑜  (B) 馬英九、蔡英文、林瑞雄\r\n",
+			"(B) 吳敦義、蘇嘉全、林瑞雄  (D) 馬英九、蔡英文、宋楚瑜\r\n",
+		4,
+		0
+	},
+	{
+		"下列哪一個排序演算法的平均時間複雜度不是O(nlogn)\r\n"
+			"(A) quick sort (B) selection sort (C) radix sort (D) merge sort\r\n",
+		2,
+		0
 	},
 	{
 		"請問這是哪部作品的歌曲?\r\n"
@@ -43,10 +86,30 @@ code struct Question {
 		2
 	},
 	{
+		"下列哪一個問題是decidable?\r\n"
+			"(A) Halting problem (B) post correspondence problem\r\n",
+			"(C) a turing machine determines whehter two turing marchines are equal\r\n",
+			"(D) clique problem\r\n",
+		4,
+		0
+	},
+	{
+		"在WOW的故事中，哪一位是被遺忘者的領導人\r\n"
+			"(A) Sylvanas (B) Thrall (C) Malfurion (D) Arthas\r\n",
+		1,
+		0
+	},
+	{
 		"請問這是哪部作品的歌曲?\r\n"
 			"(A) 那些年，我們一起追的女孩  (B) 小資女孩向前衝  (C) 犀利人妻  (D) 我可能不會愛你\r\n",
 		2,
 		3
+	},
+	{
+		"請問下列哪部作品預計在2012年1月開撥?\r\n"
+			"(A) 蘿球社 (B) 要聽爸爸的話 (C) 魔法少女小圓 (D) 我的朋友很少\r\n",
+		2,
+		0
 	},
 };
 
