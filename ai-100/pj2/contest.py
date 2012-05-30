50ed15cb03cef6c65f5d9dc43963aac53ed81509
Shaform <shaform@gmail.com>
2012-05-30 12:18:44 +0800

    Add contest evaluation.

---
+++ ai-100/pj2/contest.py
@@ -0,0 +1,83 @@
+from gen import getResult
+import os
+import pickle
+
+
+LOAD_PATH = './gen_param'
+SAVE_PATH = './result_param'
+
+# check directory if not exists
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+
+nextFId = 0
+nextSId = 0
+
+# read index files
+with open(LOAD_PATH + '/bestFId', 'r') as f:
+    nextFId = pickle.load(f)
+with open(LOAD_PATH + '/bestSId', 'r') as f:
+    nextSId = pickle.load(f)
+
+f = open(SAVE_PATH + '/result', 'a')
+
+def loadFirstParams(FId):
+    with open(LOAD_PATH + '/bestF%d' % FId, 'r') as f:
+        return pickle.load(f)
+
+def loadSecondParams(FId):
+    with open(LOAD_PATH + '/bestS%d' % FId, 'r') as f:
+        return pickle.load(f)
+
+
+def checkParam(aParam, bParam, hand):
+    t = getResult(aParam, aParam,
+            bParam, bParam, hand)
+    if t > 0:
+        return t;
+    else:
+        return None
+
+def checkPair(aId, aRole, bId, bRole, hand):
+    aParam = []
+    if aRole == 0:
+        aParam = loadFirstParams(aId)
+    else:
+        aParam = loadSecondParams(aId)
+    bParam = []
+    if bRole == 0:
+        bParam = loadFirstParams(bId)
+    else:
+        bParam = loadSecondParams(bId)
+
+    return checkParam(aParam, bParam, hand)
+
+def outputLine(line):
+    print line
+    f.write(line + '\n')
+    f.flush()
+
+ROUND_NUM = 5
+cRound = 0
+
+
+while cRound < nextFId or cRound < nextSId:
+    outputLine('round %d' % cRound)
+
+    maxIds = [min(cRound+ROUND_NUM, nextFId), min(cRound+ROUND_NUM, nextSId)]
+
+    for p in range(2):
+        for q in range(2):
+
+            for i in range(cRound, maxIds[p]):
+                r = [0, 0]
+                for j in range(cRound, maxIds[q]):
+                    t = checkPair(i, p, j, q, p)
+                    if t:
+                        outputLine('[%d,%d] wins with %d' % (p, i, t))
+                        r = [r[0]+1, r[1]+t]
+                outputLine('[%d,%d] wins %d with %d against %d' % (p, i, r[0], r[1], q))
+
+    cRound = cRound + ROUND_NUM
+
+f.close()
--- ai-100/pj2/gen.py
+++ ai-100/pj2/gen.py
@@ -10,15 +10,17 @@ import signal
  
 
 MAX = 1000000
+SMAX = 50
 SAVE_PATH = './gen_param'
+PSMALL = 4
 
 nextFId = 0
 nextSId = 0
 
 bestResults = [-12, -12]
 bestParams = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
+        [0, 0, 0, 0, 10],
+        [0, 0, 0, 0, 10],
         ]
 
 
@@ -50,15 +52,22 @@ def genRandomParams(params):
     gened = []
     for p in params:
         gened.append(random.randint(0, MAX))
+    gened[PSMALL] = random.randint(1, SMAX)
     return gened
 
 def genNewParams(params):
     gened = list(params)
     idx = random.randint(0, len(params)-1)
     p = gened[idx]
-    gened[idx] = random.randint(max(0, p/2), min(p+p/2, MAX))
-    if random.randint(0, 100) > 80:
-        gened[idx] = random.randint(0, MAX)
+    if idx == PSMALL:
+        gened[idx] = random.randint(max(1, p/2), min(p+p/2, SMAX))
+        if random.randint(0, 100) > 80:
+            gened[idx] = random.randint(1, SMAX)
+    else:
+        gened[idx] = random.randint(max(0, p/2), min(p+p/2, MAX))
+        if random.randint(0, 100) > 80:
+            gened[idx] = random.randint(0, MAX)
+
     return gened
 
 def writeParams(params, player, fname):
@@ -67,21 +76,16 @@ def writeParams(params, player, fname):
             f.write('%d ' % p)
 
 def getResult(fp1, sp1, fp2, sp2, hand):
+
+    subprocess.call(["./clear_files"], shell=True)
     time.sleep(1)
+
     writeParams(fp1, 1, 'first')
     writeParams(sp1, 1, 'second')
     writeParams(fp2, 2, 'first')
     writeParams(sp2, 2, 'second')
 
-    subprocess.call(["./clear_files"], shell=True)
-    time.sleep(1)
-
-    if hand == 0:
-        fPlayer = '2'
-    else:
-        fPlayer = '1'
-
-    b = Popen(['java', 'Umpire', fPlayer, 'false', '600'], stdout=subprocess.PIPE)
+    b = Popen(['java', 'Umpire', '%d' % (hand+1), 'false', '600'], stdout=subprocess.PIPE)
     c = Popen(['./proj2_client', '1', 'first', 'second'],
             cwd=os.path.abspath('./player1/'), stdout=subprocess.PIPE)
     d = Popen(['./proj2_client', '2', 'first', 'second'],
@@ -105,7 +109,7 @@ def getResult(fp1, sp1, fp2, sp2, hand):
             m = re.match('player(\d) is winner, remain (\d+)', line)
             if m is not None:
                 result = int(m.group(2))
-                if int(m.group(1)) == 1:
+                if int(m.group(1)) == 2:
                     result = -result
                 break
 
@@ -124,124 +128,129 @@ def getResult(fp1, sp1, fp2, sp2, hand):
         d.wait()
         b.wait()
 
-    time.sleep(3)
+    time.sleep(2)
     return result
 
+if __name__ == '__main__':
 # set up environments
 
 # check directory if not exists
-if not os.path.exists(SAVE_PATH):
-    os.makedirs(SAVE_PATH)
+    if not os.path.exists(SAVE_PATH):
+        os.makedirs(SAVE_PATH)
 
 # check index files
-if not os.path.exists(SAVE_PATH + '/bestFId'):
-    with open(SAVE_PATH + '/bestFId', 'w') as f:
-        pickle.dump(0, f)
+    if not os.path.exists(SAVE_PATH + '/bestFId'):
+        with open(SAVE_PATH + '/bestFId', 'w') as f:
+            pickle.dump(0, f)
 
-if not os.path.exists(SAVE_PATH + '/bestSId'):
-    with open(SAVE_PATH + '/bestSId', 'w') as f:
-        pickle.dump(0, f)
+    if not os.path.exists(SAVE_PATH + '/bestSId'):
+        with open(SAVE_PATH + '/bestSId', 'w') as f:
+            pickle.dump(0, f)
 
 # read index files
-with open(SAVE_PATH + '/bestFId', 'r') as f:
-    nextFId = pickle.load(f)
-with open(SAVE_PATH + '/bestSId', 'r') as f:
-    nextSId = pickle.load(f)
+    with open(SAVE_PATH + '/bestFId', 'r') as f:
+        nextFId = pickle.load(f)
+    with open(SAVE_PATH + '/bestSId', 'r') as f:
+        nextSId = pickle.load(f)
 
 
-def calcBestResults():
-    global bestResults
-    for h in [0, 1]:
-        bestResults[h] = getResult(bestParams[0], bestParams[1],
-                bestParams[0], bestParams[1], h)
+    def calcBestResults():
+        global bestResults
+        for h in [0, 1]:
+            bestResults[h] = getResult(bestParams[0], bestParams[1],
+                    bestParams[0], bestParams[1], h)
 
-def checkSame(params, h):
-    return False
-    print 'check same'
-    a = getResult(bestParams[h], bestParams[h],
-            params[h], params[h], 0)
-    b = getResult(bestParams[h], bestParams[h],
-            params[h], params[h], 1)
-    if a+b > 0:
-        print 'better'
-        return True
-    else:
-        print 'worse'
+    def checkSame(params, h):
         return False
+        print 'check same'
+        a = getResult(params[h], params[h],
+                bestParams[h], bestParams[h], 0)
+        b = getResult(params[h], params[h],
+                bestParams[h], bestParams[h], 1)
+        if a+b > 0:
+            print 'better'
+            return True
+        else:
+            print 'worse'
+            return False
 
 
-if nextFId > 0 and nextSId > 0:
-    with open(SAVE_PATH + '/bestF%d' % (nextFId-1), 'r') as f:
-        bestParams[0] = pickle.load(f)
-    with open(SAVE_PATH + '/bestS%d' % (nextSId-1), 'r') as f:
-        bestParams[1] = pickle.load(f)
-    calcBestResults()
+    if nextFId > 0 and nextSId > 0:
+        with open(SAVE_PATH + '/bestF%d' % (nextFId-1), 'r') as f:
+            bestParams[0] = pickle.load(f)
+        with open(SAVE_PATH + '/bestS%d' % (nextSId-1), 'r') as f:
+            bestParams[1] = pickle.load(f)
+        calcBestResults()
 
-print bestResults
+    print bestResults
 
 # main loop
 
-tryNum = 0
-switchNum = 0
-testHand = 1
+    tryNum = 0
+    switchNum = 0
+    testHand = 0
 
-bParams = list(bestParams)
-cParams = list(bParams)
-bResults = list(bestResults)
-while True:
-    # gen new result
-    cParams[testHand] = genNewParams(bParams[testHand])
-    # random restart
-    if tryNum > 10:
-        if random.randint(0, 100) > 60:
-            cParams[testHand] = genRandomParams(cParams[testHand])
-            # reset try number
-            tryNum = 0
-            # always accept new result
-            bResults[testHand] = -12
+    bParams = list(bestParams)
+    cParams = list(bParams)
+    bResults = list(bestResults)
+    while True:
+        # gen new result
+        cParams[testHand] = genNewParams(bParams[testHand])
+        # random restart
+        if tryNum > 10:
+            if random.randint(0, 100) > 60:
+                cParams[testHand] = genRandomParams(cParams[testHand])
+                # reset try number
+                tryNum = 0
+                # always accept new result
+                bResults[testHand] = -12
 
-    print 'gen param'
-    print cParams[testHand]
-    print 'for hand%d' % testHand
-    print 'now best'
-    print bestResults
-    print 'now current best'
-    print bResults
+        print 'gen param'
+        print cParams[testHand]
+        print 'for hand%d' % testHand
+        print 'now best'
+        print bestResults
+        print 'now current best'
+        print bResults
 
-    result = getResult(bestParams[0], bestParams[1],
-        cParams[0], cParams[1], testHand)
-    print 'Result %d by' % result
-    print cParams[testHand]
-    
-    if result != 0 and result > bResults[testHand]:
+        result = getResult(cParams[0], cParams[1],
+                bestParams[0], bestParams[1], testHand)
+        print 'Result %d by' % result
+        print cParams[testHand]
+        
+        if result != 0 and result > bResults[testHand]:
 
-        if result > bResults[testHand]:
-            tryNum = 0
+            if result > bResults[testHand]:
+                tryNum = 0
 
-        bResults[testHand] = result
-        bParams[testHand] = list(cParams[testHand])
+            bResults[testHand] = result
+            bParams[testHand] = list(cParams[testHand])
 
-        if (bResults[testHand] > bestResults[testHand]
-                or (result == bestResults[testHand]
-                    and checkSame(cParams, testHand))):
-            print 'New Result'
+            if (bResults[testHand] > bestResults[testHand]
+                    or (result == bestResults[testHand]
+                        and checkSame(cParams, testHand))):
+                print 'New Result'
+                if testHand == 0:
+                    saveFirst(cParams[0])
+                else:
+                    saveSecond(cParams[1])
+
+                # force switch
+                switchNum = switchNum + 10
+                calcBestResults()
+                # start from best
+                bParams = list(bestParams)
+                cParams = list(bParams)
+                bResults = list(bestResults)
+
+        else:
+            tryNum = tryNum + 1
+            switchNum = switchNum + 1
+
+        if switchNum > 20:
             if testHand == 0:
-                saveFirst(cParams[0])
+                m = nextFId > nextSId
             else:
-                saveSecond(cParams[1])
-
-            # force switch
-            switchNum = switchNum + 100
-            calcBestResults()
-            # start from best
-            bParams = list(bestParams)
-            cParams = list(bParams)
-            bResults = list(bestResults)
-
-    else:
-        tryNum = tryNum + 1
-        switchNum = switchNum + 1
-
-    if switchNum > 100:
-        if bResults[testHand] > 0 or switchNum > 200:
-            testHand = (testHand + 1) % 2
+                m = nextSId > nextFId
+            if m or switchNum > 30:
+                testHand = (testHand + 1) % 2
